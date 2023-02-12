8860310e4b7d40c416d577f181265b40f9c9386b
tranduydat <trduydat@gmail.com>
2023-02-12 20:36:28 +0700

    Add Log4j support

--- src/main/java/com/tranduydat/boilerplate/App.java
+++ src/main/java/com/tranduydat/boilerplate/App.java
@@ -1,13 +1,19 @@
 package com.tranduydat.boilerplate;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static final Logger logger = LogManager.getLogger(App.class);
     public static void main( String[] args )
     {
+        logger.error("Run!");
+        logger.fatal("Failed");
         System.out.println( "Hello World!" );
     }
 }
